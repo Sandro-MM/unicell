e259dd5023eb19eecf541497dd163ab9830eae87
Your Name <ssandroo35@gmail.com>
2024-11-15 17:49:41 +0400

    help page init

--- src/app/pages/help-page/help-page.component.ts
+++ src/app/pages/help-page/help-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {TextareaModule} from 'primeng/textarea';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-help-page',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    TextareaModule,
+    TranslatePipe
+  ],
   templateUrl: './help-page.component.html',
   styleUrl: './help-page.component.css'
 })
